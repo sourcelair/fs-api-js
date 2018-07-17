789da60ed93fecda789a572d43d79054d229945b
irokas <akasidiari@inf.uth.gr>
2018-07-17 14:13:06 +0300

    Clear code

--- src/main.js
+++ src/main.js
@@ -4,58 +4,31 @@ function strComp (str1, str2) {
   }
 }
 
+function checkAndPush(inputElement, liArray) {
+  const tempLiItem = document.createElement("li");
+  tempLiItem.textContent = inputElement.name;
+  for (j=0; j<liArray.length; j++){
+    if (strComp(inputElement.name.toUpperCase(), liArray[j].textContent.toUpperCase()))
+      break;
+  }
+  return j;
+}
+
 function renderInput(input, container){
   const dirItem = [];
   const fileItem = [];
 
   const dirList = document.createElement("ul");
   const fileList = document.createElement("ul");
-  
+
   var i, j;
   for (i=0; i<input.length; i++) {
-    if (input[i].type === "directory"){
-      const tempDirItem = document.createElement("li");
-      tempDirItem.textContent = input[i].name;
-      j=0;
-
-      if(dirItem.length!==0){
-        for (j=0; j<dirItem.length; j++){
-          const tempInput = input[i].name;
-          const str1 = tempInput.toUpperCase();
-
-          const tempD = dirItem[j].textContent;
-          const str2 = tempD.toUpperCase();
-
-          if (strComp(str1, str2)){
-            break;
-          }
-        }
-      }
-      dirItem.splice(j, 0, tempDirItem);
-    }
-    else if (input[i].type === "file"){
-      const tempFileItem = document.createElement("li");
-      tempFileItem.textContent = input[i].name;
-      j=0;
-
-      if(fileItem.length!==0){
-        for (j=0; j<fileItem.length; j++){
-          const tempInput = input[i].name;
-          const str1 = tempInput.toUpperCase();
-
-          const tempF = fileItem[j].textContent;
-          const str2 = tempF.toUpperCase();
-
-          if (strComp(str1, str2)){
-            break;
-          }
-        }
-      }
-      fileItem.splice(j, 0, tempFileItem);
-    }
-    else {
-      console.log("Error in type of file");
-    }
+    const tempItem = document.createElement("li");
+    tempItem.textContent = input[i].name;
+    if (input[i].type === "directory")
+      dirItem.splice(checkAndPush(input[i], dirItem), 0, tempItem);
+    else 
+      fileItem.splice(checkAndPush(input[i], fileItem), 0, tempItem);
   }
 
   for (i=0; i<dirItem.length; i++) {
@@ -67,4 +40,3 @@ function renderInput(input, container){
   container.appendChild(dirList);
   container.appendChild(fileList);
 }
-
