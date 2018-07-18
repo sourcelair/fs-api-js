92f4dd72ebcd427a41c2b7a712b96615a0235ebe
irokas <akasidiari@inf.uth.gr>
2018-07-18 14:07:04 +0300

    Included both the directories and files in the same list

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-function iterator(array) {
+function convertItemsToUnorderedList(listItems) {
   const ulList = document.createElement("ul");
-  array.map(a => {
+  listItems.map(a => {
     const liItem = document.createElement("li");
     liItem.textContent = a.name;
     ulList.appendChild(liItem);
@@ -8,18 +8,16 @@ function iterator(array) {
   return ulList;
 }
 
+function alphabeticCompare(a, b) {
+  return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
+}
+
 function renderInput(input, container) {
   const dirItem = input.filter(inputEl => inputEl.type === "directory"),
-    fileItem = input.filter(inputEl => inputEl.type === "file");
-  dirItem.sort(function(a, b) {
-    return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
-  });
-  fileItem.sort(function(a, b) {
-    return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
-  });
-
-  dirList = iterator(dirItem);
-  fileList = iterator(fileItem);
-  container.appendChild(dirList);
-  container.appendChild(fileList);
+        fileItem = input.filter(inputEl => inputEl.type === "file");
+  dirItem.sort(alphabeticCompare);
+  fileItem.sort(alphabeticCompare);
+  const listItems = dirItem.concat(fileItem);
+  ulList = convertItemsToUnorderedList(listItems);
+  container.appendChild(ulList);
 }
